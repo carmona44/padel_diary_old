79802e073edde7c3c4a19fefa060ba92b93317da
Daniel <dcacarmona44@gmail.com>
2021-01-02 19:08:32 +0100

    Added modals

--- lib/widgets/add_floating_button.dart
+++ lib/widgets/add_floating_button.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:padel_diary/widgets/modals/fit_modal.dart';
+import 'package:padel_diary/widgets/modals/floating_modal.dart';
 
 class AddFloatingButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(child: Icon(Icons.add), onPressed: () {});
+    return FloatingActionButton(
+      child: Icon(Icons.add),
+      onPressed: () {
+        showFloatingModalBottomSheet(
+          context: context,
+          builder: (context) => ModalFit(),
+        );
+      },
+    );
   }
 }
