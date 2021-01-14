43095d585f2f8979d88ec1e94d578beb9f8dc839
Daniel <dcacarmona44@gmail.com>
2021-01-14 20:36:14 +0100

    Switched player list from matches to profile page

--- lib/pages/matches_page.dart
+++ lib/pages/matches_page.dart
@@ -1,20 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:padel_diary/providers/player_provider.dart';
-import 'package:provider/provider.dart';
 
 class MatchesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final playerProvider = Provider.of<PlayerProvider>(context);
-    final players = playerProvider.players;
-
-    return ListView.builder(
-        itemCount: players.length,
-        itemBuilder: (_, i) => ListTile(
-              leading: Icon(Icons.ac_unit),
-              title: Text('${players[i].name} ${players[i].surname}'),
-              subtitle: Text('${players[i].position}'),
-              onTap: () => {},
-            ));
+    return Center(
+      child: Text('Matches page'),
+    );
   }
 }
--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:padel_diary/providers/player_provider.dart';
+import 'package:provider/provider.dart';
 
 class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile page'),
-    );
+    final playerProvider = Provider.of<PlayerProvider>(context);
+    final players = playerProvider.players;
+
+    return ListView.builder(
+        itemCount: players.length,
+        itemBuilder: (_, i) => ListTile(
+              leading: Icon(Icons.ac_unit),
+              title: Text('${players[i].name} ${players[i].surname}'),
+              subtitle: Text('${players[i].position}'),
+              onTap: () => {},
+            ));
   }
 }
