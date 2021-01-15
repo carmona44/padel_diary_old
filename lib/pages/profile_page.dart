3dbcb44794c9fc900ccf424561e4c8d5ba682232
Daniel <dcacarmona44@gmail.com>
2021-01-15 21:56:44 +0100

    Create list of created items in profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:padel_diary/models/ball_model.dart';
+import 'package:padel_diary/models/club_model.dart';
+import 'package:padel_diary/models/player_model.dart';
+import 'package:padel_diary/models/tournament_model.dart';
 import 'package:padel_diary/providers/player_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -8,13 +12,77 @@ class ProfilePage extends StatelessWidget {
     final playerProvider = Provider.of<PlayerProvider>(context);
     final players = playerProvider.players;
 
-    return ListView.builder(
-        itemCount: players.length,
-        itemBuilder: (_, i) => ListTile(
-              leading: Icon(Icons.ac_unit),
-              title: Text('${players[i].name} ${players[i].surname}'),
-              subtitle: Text('${players[i].position}'),
-              onTap: () => {},
-            ));
+    return ListView(
+      children: [
+        ..._playerList(players),
+        ..._clubList([]),
+        ..._tournamentList([]),
+        ..._ballList([]),
+      ],
+    );
+  }
+
+  List<Widget> _playerList(List<Player> players) {
+    List<Widget> widgets = [];
+
+    widgets..add(ListTile(title: Text('Jugadores')))..add(Divider());
+
+    for (var i = 0; i < players.length; i++) {
+      widgets.add(ListTile(
+        leading: Icon(Icons.ac_unit),
+        title: Text('${players[i].name} ${players[i].surname}'),
+        subtitle: Text('${players[i].position}'),
+      ));
+    }
+
+    return widgets;
+  }
+
+  List<Widget> _clubList(List<Club> clubs) {
+    List<Widget> widgets = [];
+
+    widgets..add(ListTile(title: Text('Clubes')))..add(Divider());
+
+    for (var i = 0; i < clubs.length; i++) {
+      widgets.add(ListTile(
+        leading: Icon(Icons.ac_unit),
+        title: Text('${clubs[i].name}'),
+        subtitle: Text('${clubs[i].city}'),
+      ));
+    }
+
+    return widgets;
+  }
+
+  List<Widget> _tournamentList(List<Tournament> tournament) {
+    List<Widget> widgets = [];
+
+    widgets..add(ListTile(title: Text('Torneos')))..add(Divider());
+
+    for (var i = 0; i < tournament.length; i++) {
+      widgets.add(ListTile(
+        leading: Icon(Icons.ac_unit),
+        title: Text('${tournament[i].name}'),
+        subtitle: Text('${tournament[i].city}'),
+      ));
+    }
+
+    return widgets;
+  }
+
+  List<Widget> _ballList(List<Ball> balls) {
+    List<Widget> widgets = [];
+
+    widgets..add(ListTile(title: Text('Bolas')))..add(Divider());
+
+    for (var i = 0; i < balls.length; i++) {
+      widgets.add(ListTile(
+        leading: Icon(Icons.ac_unit),
+        title: Text('${balls[i].brand}'),
+        subtitle: Text('${balls[i].model}'),
+      ));
+    }
+
+    return widgets;
   }
 }
