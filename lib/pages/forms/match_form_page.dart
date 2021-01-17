366608fc5f93b108953cac03594ec4c0449bd6df
Daniel <dcacarmona44@gmail.com>
2021-01-17 14:04:16 +0100

    Started with match form page + added datepicker

--- lib/pages/forms/match_form_page.dart
+++ lib/pages/forms/match_form_page.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:padel_diary/models/match_model.dart';
+import 'package:padel_diary/providers/match_provider.dart';
+import 'package:provider/provider.dart';
 
-class MatchFormPage extends StatelessWidget {
+class MatchFormPage extends StatefulWidget {
+  @override
+  _MatchFormPageState createState() => _MatchFormPageState();
+}
+
+class _MatchFormPageState extends State<MatchFormPage> {
+  final formKey = GlobalKey<FormState>();
+  Match matchModel = Match();
+
+  TextEditingController _dateController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,12 +24,86 @@ class MatchFormPage extends StatelessWidget {
         child: Container(
           padding: EdgeInsets.all(15.0),
           child: Form(
+            key: formKey,
             child: Column(
-              children: [],
+              children: [
+                /*_inputTeamALeft(),
+                _inputTeamARight(),
+                _inputTeamAFirstSet(),
+                _inputTeamASecondSet(),
+                _inputTeamAThirdSet(),
+                _inputTeamBLeft(),
+                _inputTeamBRight(),
+                _inputTeamBFirstSet(),
+                _inputTeamBSecondSet(),
+                _inputTeamBThirdSet(),*/
+                //_inputClub(),
+                //_inputTournament(),
+                //_inputBall(),
+                _dateInput(context),
+                //_effortInput(),
+                //_commentsInput(),
+                //_temperatureInput(),
+                //_mvpInput(),
+                //_durationInput(),
+                SizedBox(height: 25.0),
+                _submitButton()
+              ],
             ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _dateInput(BuildContext context) {
+    return TextFormField(
+      enableInteractiveSelection: false,
+      controller: _dateController,
+      decoration: InputDecoration(hintText: 'Fecha', labelText: 'Fecha'),
+      onTap: () {
+        FocusScope.of(context).requestFocus(new FocusNode());
+        _selectDate(context);
+      },
+    );
+  }
+
+  _selectDate(BuildContext context) async {
+    DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2000),
+      lastDate: DateTime.now(),
+    );
+
+    if (picked != null) {
+      setState(() {
+        matchModel.date = picked.microsecondsSinceEpoch;
+        _dateController.text = picked.toString();
+      });
+    }
+  }
+
+  Widget _submitButton() {
+    return RaisedButton.icon(
+      icon: Icon(Icons.save),
+      label: Text('Crear partido'),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      onPressed: _submit,
+    );
+  }
+
+  void _submit() {
+    if (!formKey.currentState.validate()) return;
+
+    formKey.currentState.save();
+
+    final matchProvider = Provider.of<MatchProvider>(context, listen: false);
+    matchProvider.createMatch(matchModel);
+
+    formKey.currentState.reset();
+    setState(() {
+      matchModel = Match();
+    });
   }
 }
