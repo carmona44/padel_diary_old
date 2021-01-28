b45f9d0e794c61b42712e77570979bdd20be340a
Daniel <dcacarmona44@gmail.com>
2021-01-28 20:25:25 +0100

    First match widget schema

--- lib/pages/matches_page.dart
+++ lib/pages/matches_page.dart
@@ -7,4 +7,33 @@ class MatchesPage extends StatelessWidget {
       child: Text('Matches page'),
     );
   }
+
+  Widget _createMatchContainer() {
+    return Card(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Text('Nombre1'),
+              Text('Nombre2'),
+              Icon(Icons.ac_unit), //SizedBox()
+              Text('6'),
+              Text('7'),
+              Text('6'),
+            ],
+          ),
+          Row(
+            children: [
+              Text('Nombre3'),
+              Text('Nombre4'),
+              Icon(Icons.ac_unit), //SizedBox()
+              Text('4'),
+              Text('5'),
+              Text('3'),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
