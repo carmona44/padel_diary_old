5aabc27f793be46d5d2f0d1447c0c0b2bb5dae38
Daniel <dcacarmona44@gmail.com>
2021-01-30 13:49:38 +0100

    Match page style

--- lib/pages/matches_page.dart
+++ lib/pages/matches_page.dart
@@ -3,34 +3,69 @@ import 'package:flutter/material.dart';
 class MatchesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Matches page'),
+    return ListView.builder(
+      itemCount: 10,
+      itemBuilder: (context, index) => _createMatchContainer(),
     );
   }
 
   Widget _createMatchContainer() {
-    return Card(
+    TextStyle _setResultStyle = TextStyle(fontSize: 24.0);
+    return Container(
+      padding: EdgeInsets.all(10.0),
       child: Column(
         children: [
-          Row(
-            children: [
-              Text('Nombre1'),
-              Text('Nombre2'),
-              Icon(Icons.ac_unit), //SizedBox()
-              Text('6'),
-              Text('7'),
-              Text('6'),
-            ],
-          ),
-          Row(
-            children: [
-              Text('Nombre3'),
-              Text('Nombre4'),
-              Icon(Icons.ac_unit), //SizedBox()
-              Text('4'),
-              Text('5'),
-              Text('3'),
-            ],
+          Text('28/01/2021'),
+          Card(
+            elevation: 12.0,
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 20.0),
+              child: Table(
+                columnWidths: {0: FractionColumnWidth(.40)},
+                defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+                children: [
+                  TableRow(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(left: 5.0),
+                        child: Text('Daniel / Jose Javier',
+                            textAlign: TextAlign.center),
+                      ),
+                      Icon(Icons.ac_unit), //SizedBox()
+                      Text('6',
+                          style: _setResultStyle, textAlign: TextAlign.center),
+                      Text('7',
+                          style: _setResultStyle, textAlign: TextAlign.center),
+                      Text('6',
+                          style: _setResultStyle, textAlign: TextAlign.center),
+                    ],
+                  ),
+                  TableRow(children: [
+                    SizedBox(height: 20.0),
+                    SizedBox(),
+                    SizedBox(),
+                    SizedBox(),
+                    SizedBox()
+                  ]),
+                  TableRow(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(left: 5.0),
+                        child:
+                            Text('Jesús / Luis', textAlign: TextAlign.center),
+                      ),
+                      Icon(Icons.ac_unit), //SizedBox()
+                      Text('4',
+                          style: _setResultStyle, textAlign: TextAlign.center),
+                      Text('5',
+                          style: _setResultStyle, textAlign: TextAlign.center),
+                      Text('3',
+                          style: _setResultStyle, textAlign: TextAlign.center),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
